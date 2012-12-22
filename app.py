d7c471b85438216851cf10afb713e1a85d3a7e53
河村 雅人 <kawamuramasato@softbank126101149251.bbtec.net>
2012-12-22 14:30:17 +0900

    add template

--- app.py
+++ app.py
@@ -1,16 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'HELLO World!'
-
+    return render_template('index.html', name=None)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT',5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-
-
-
