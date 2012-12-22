847e6cf4156402778434a6bbe4dc059b33d89c40
河村 雅人 <kawamuramasato@softbank126101149251.bbtec.net>
2012-12-22 15:22:51 +0900

    emend template

--- app.py
+++ app.py
@@ -1,11 +1,33 @@
 import os
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+def index():
     return render_template('index.html', name=None)
+
+
+@app.route('/grouping/', methods=['POST', 'GET'])
+def proc_post_request():
+    """ 
+    dispatch RPC request
+
+    """
+    if request.method == 'POST':
+        total = request.form['total']
+        group = request.form['group']
+        result = grouping(total, group)
+        #result=search_zipcode(request.form['zipcode'])
+        return result
+    else:
+        return 'Please PSOT Method'
+
+def grouping(total, group):
+    '''
+    '''
+    return "%s,%s" %(total,group)
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
