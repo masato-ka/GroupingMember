ff35751fc3b57994d58aa197fdbe37d356825e3b
河村 雅人 <kawamuramasato@softbank126101149251.bbtec.net>
2012-12-22 15:59:58 +0900

    emend template

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+# -*- coding: utf-8 -*-
 import os
+import random
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -10,10 +12,11 @@ def index():
 
 @app.route('/grouping/', methods=['POST', 'GET'])
 def proc_post_request():
-    """ 
+    ''' 
     dispatch RPC request
 
-    """
+    '''
+
     if request.method == 'POST':
         total = request.form['total']
         group = request.form['group']
@@ -25,9 +28,34 @@ def proc_post_request():
 
 def grouping(total, group):
     '''
-    '''
-    return "%s,%s" %(total,group)
+    全体の人数とグループ数から各グループの数を求める
+    各グループにランダムに人数を配置する。
 
+    '''
+    try:
+        number_total = int(total)
+        number_group = int(group)
+    except:
+        return None
+
+    number_per_group = number_total / number_group
+
+    persons = range(0, number_total)
+    groups = range(0, number_group)
+
+    # create group
+    group_dic = {}
+    for group in groups:
+        group_dic['%s', group] = []
+
+    random.shuffle(persons)
+    for key in group_dic.keys():
+        for enumrate in number_per_group:
+            group_dic[key].append(persons.pop())
+            if not len(persons):
+                break
+        
+    return json.dumps(group_dic, ensure_ascii=False)
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
